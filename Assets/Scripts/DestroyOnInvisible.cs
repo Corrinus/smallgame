78cfe1a8ef71e2225033f66ccd81d45220ee0b9d
Nick Maynard <Nick Maynard>
2016-07-17 22:29:15 -0500

    Added randomly spawning pillars as obstacle

---
+++ Assets/Scripts/DestroyOnInvisible.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnInvisible : MonoBehaviour {
+
+	//Destroy the object once it's not on screen
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
