077d315f53da363f6fefc13a664a517ad6bd0934
Nick Maynard <Nick Maynard>
2016-07-17 21:08:30 -0500

    Added skybox & basic movement via scrolling texture

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Move : MonoBehaviour {
 
     public float speed = 1.0f;
+    public float speedz = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,6 @@ public class Move : MonoBehaviour {
 	void Update () {
         //Moves object left or right based on input
         transform.position += Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        transform.position += Vector3.forward * Time.deltaTime * speedz;
     }
 }
